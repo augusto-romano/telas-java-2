13274f069191c6d1a3c75b806339c9a273043539
Augusto Romano Segantini <126214133+augusto-romano@users.noreply.github.com>
2023-03-12 13:25:32 -0300

    Add files via upload

---
+++ src/main/java/br/edu/unifio/segundo/projeto/SegundoProjeto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.unifio.segundo.projeto;
+
+/**
+ *
+ * @author Delfino
+ */
+public class SegundoProjeto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
